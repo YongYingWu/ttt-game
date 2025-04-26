11209a048758084c5d9446d140641aa7668e984c
1093661893 <1093661893@qq.com>
2025-04-26 18:04:32 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: './',
+  base: '/ttt-game/',
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
